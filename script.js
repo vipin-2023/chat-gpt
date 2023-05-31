1f0e6b6aa27536f34ccecb5c436d9f9051fd3bdb
vipin-2023 <githublogin321@gmail.com>
2023-05-31 12:54:27 +0530

    finished

--- script.js
+++ script.js
@@ -2,16 +2,32 @@ const chatInput = document.querySelector("#chat-input")
 const sendButton = document.querySelector("#send-btn")
 const chatContainer = document.querySelector(".chat-container")
 const themeButton = document.querySelector("#theme-btn");
+const deleteButton = document.querySelector("#delete-btn");
+
+let userText = null;
 
 const API_KEY = "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";
-
+const initialHeight = chatInput.scrollHeight;
 const loadDataFromLoacalstorage =()=>{
-  chatContainer.innerHTML = localStorage.getItem("all-chats");
+  const themeColor = localStorage.getItem("theme-color");
+
+  document.body.classList.toggle("light-mode",themeColor==='sunny-outline');
+  var element = themeButton.querySelector('ion-icon[name]');
+  localStorage.setItem("theme-color",element.getAttribute("name"))
+  themeButton.innerHTML = document.body.classList.contains("light-mode")?`<ion-icon name="contrast-outline">`:`<ion-icon name="sunny-outline">`
+  
+  const defaultText =`<div class="default-text">
+    <h1>ChatGpt</h1>
+    <p>Start a conversation and explore the power of AI<br>Your chats goes here .</p>
+  </div>`
+
+
+  chatContainer.innerHTML = localStorage.getItem("all-chats") || defaultText;
+  chatContainer.scrollTo(0,chatContainer.scrollHeight);
 
 }
 loadDataFromLoacalstorage();
 
-let userText = null;
 
 const createElement = (html,className)=>{
   const chatDiv = document.createElement("div")
@@ -48,13 +64,13 @@ try {
   pElement.textContent = response.choices[0].text.trim();
 
 } catch (error) {
-  console.log(error)
-  console.log("inside c")
+  pElement.textContent = "Ooops ! Something went wrong while retrieving the response.Please try angain.";
 
 }
 incomingChatDiv.querySelector(".typing-animation").revome();
 incomingChatDiv.querySelector(".chat-details").appendChild(pElement);
-localStorage.setItem("all-chats",chatContainer.innerHTML)
+chatContainer.scrollTo(0,chatContainer.scrollHeight);
+localStorage.setItem("all-chats",chatContainer.innerHTML);
 
 }
 const copyResponse = (copyBtn)=>{
@@ -77,7 +93,9 @@ const showTypingAnimation =()=>{
     <span onclick="copyResponse(this)" ><ion-icon class="ionic-icons" name="copy-outline"></ion-icon></span>
 </div>`
 const incomingChatDiv = createElement(html,"incoming")
-chatContainer.appendChild(incomingChatDiv)
+chatContainer.appendChild(incomingChatDiv);
+chatContainer.scrollTo(0,chatContainer.scrollHeight);
+
 getChatResponse(incomingChatDiv)
 }
 
@@ -85,6 +103,10 @@ getChatResponse(incomingChatDiv)
 const handleOutgoingChat = () => {
     userText = chatInput.value.trim()
     if(!userText) return;
+
+  chatInput.value="";
+  chatInput.style.height = `${initialHeight}px `;
+
     const html = ` <div class="chat-content">
                    <div class="chat-details">
                    <img src="/assets/avatar.jpeg" alt="user">
@@ -93,15 +115,40 @@ const handleOutgoingChat = () => {
                    </div>`
 const outgoingChatDiv = createElement(html,"outgoing")
 outgoingChatDiv.querySelector("p").textContent=userText;
-chatContainer.appendChild(outgoingChatDiv)
+document.querySelector('.default-text')?.remove();
+chatContainer.appendChild(outgoingChatDiv);
+chatContainer.scrollTo(0,chatContainer.scrollHeight);
 setTimeout(showTypingAnimation,500)
 }
 
 
 themeButton.addEventListener("click",()=>{
   document.body.classList.toggle("light-mode");
-  localStorage.setItem("theme-color",themeButton.innerHTML)
+ 
+
+  var element = themeButton.querySelector('ion-icon[name]');
+ 
+  localStorage.setItem("theme-color",element.getAttribute("name"))
   themeButton.innerHTML = document.body.classList.contains("light-mode")?`<ion-icon name="contrast-outline">`:`<ion-icon name="sunny-outline">`
+});
+deleteButton.addEventListener("click",()=>{
+  if(confirm("Are you sure want to delete all chats ?")){
+    localStorage.removeItem("all-chats");
+    loadDataFromLoacalstorage();
+  };
+
+})
+
+chatInput.addEventListener("input",()=>{
+  chatInput.style.height = `${initialHeight}px `;
+  chatInput.style.height = `${chatInput.scrollHeight}px `;
+
+});
+chatInput.addEventListener("keydown",(e)=>{
+  if(e.key === "Enter" && !e.shiftKey && window.innerWidth >800){
+    e.preventDefault();
+    handleOutgoingChat();
+  }
 })
 sendButton.addEventListener("click", handleOutgoingChat);
 // sendButton.addEventListener('keypress', function (e) {
